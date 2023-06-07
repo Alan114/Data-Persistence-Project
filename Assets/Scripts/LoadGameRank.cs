3dc6188c25eda4655fc66076b96aaf3204b0ab7e
Alan Pecenkovic <alanquad@yahoo.com>
2023-06-07 15:37:43 +0200

    completed scripts and objects showing best scores and player name

---
+++ Assets/Scripts/LoadGameRank.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.IO;
+using TMPro;
+
+public class LoadGameRank : MonoBehaviour
+{
+    public TextMeshProUGUI BestPlayerName;
+
+    private static int BestScore;
+    private static string BestPlayer;
+
+    private void Awake()
+    {
+        LoadBestRank();
+    }
+
+    private void SetBestPlayer()
+    {
+        if (BestPlayer == null && BestScore == 0)
+        {
+            BestPlayerName.text = "";
+        }
+        else
+        {
+            BestPlayerName.text = $"Best Score - {BestPlayer}: {BestScore}";
+        }
+
+    }
+
+    public void LoadBestRank()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            BestPlayer = data.TheBestPlayer;
+            BestScore = data.HighestScore;
+            SetBestPlayer();
+        }
+    }
+
+    [System.Serializable]
+    class SaveData
+    {
+        public int HighestScore;
+        public string TheBestPlayer;
+    }
+}
--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEditor;
+using TMPro;
 
 public class MenuUIHandler : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI PlayerNameInput;
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void SetPlayerName()
+    {
+        PlayerData.Instance.playerName = PlayerNameInput.text;
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
