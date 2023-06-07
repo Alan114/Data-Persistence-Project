39fa3c821ec82e0d2b73b9f4ffe69fb68899a3ba
Alan Pecenkovic <alanquad@yahoo.com>
2023-06-07 14:28:20 +0200

    added script to handle menu, attached handler to start button to load game

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
